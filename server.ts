9d2ed7f6f0c9b6c211152163a04a4d8158fdc754
Ved Badadhe <vbbadadhe@gmail.com>
2025-11-28 17:50:08 +0530

    Rate Limiting Added

--- server.ts
+++ server.ts
@@ -2,7 +2,11 @@ import express from "express";
 import rateLimit from "express-rate-limit";
 import { runCrons } from "./index.js";
 const app = express();
-
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 5, // limit each IP to 5 requests per windowMs
+});
+app.use(limiter);
 app.get("/", (req, res) => {
   runCrons();
   res.send("Cron Service is running & all crons executed");
